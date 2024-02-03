473ab1d83bba451e7d2608acb6b48c882fb83c92
klapouchy86 <m.jelski86@gmail.com>
2024-02-03 11:28:18 +0100

    Simple app finished

--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace PickACardUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
     }
 }
